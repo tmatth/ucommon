5c94b0d6863d887424aefad2161f1964463377b9
David Sugar <dyfet@gnutelephony.org>
2008-08-05 10:34:32 +0000

    revised or changed nt version

--- inc/ucommon/platform.h
+++ inc/ucommon/platform.h
@@ -117,7 +117,7 @@
 
 // Require for compiling with critical sections.
 #ifndef _WIN32_WINNT
-#define _WIN32_WINNT 0x0400
+#define _WIN32_WINNT 0x0501
 #endif
 
 // Make sure we're consistent with _WIN32_WINNT
