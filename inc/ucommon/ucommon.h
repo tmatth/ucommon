7a6a0264d0a75ae38b61bbd3cdce41110adb613c
David Sugar <dyfet@gnutelephony.org>
2010-06-02 13:29:46 +0000

    more consistent terminology in refering to the package as GNU uCommon C++

--- inc/ucommon/ucommon.h
+++ inc/ucommon/ucommon.h
@@ -1,13 +1,13 @@
 // Copyright (C) 2006-2008 David Sugar, Tycho Softworks.
 //
-// This file is part of GNU ucommon.
+// This file is part of GNU uCommon C++.
 //
-// GNU ucommon is free software: you can redistribute it and/or modify
+// GNU uCommon C++ is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published 
 // by the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// GNU ucommon is distributed in the hope that it will be useful,
+// GNU uCommon C++ is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
@@ -16,7 +16,7 @@
 // along with GNU ucommon.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Top level include file for the ucommon library.
+ * Top level include file for the GNU uCommon C++ core library.
  * This is the only include file you need to have in your sources; it 
  * includes the remaining header files.
  * @file ucommon/ucommon.h
@@ -24,22 +24,22 @@
 
 /**
  * @short A portable C++ threading library for embedded applications.
- * UCommon is meant as a very light-weight C++ library to facilitate using C++
- * design patterns even for very deeply embedded applications, such as for systems
- * using uclibc along with posix threading support.  For this reason, UCommon
- * disables language features that consume memory or introduce runtime overhead,
- * such as rtti and exception handling, and assumes one will mostly be linking
- * applications with other pure C based libraries rather than using the overhead
- * of the standard C++ library and other class frameworks.
+ * GNU uCommon C++ is meant as a very light-weight library to facilitate using 
+ * C++ design patterns even for very deeply embedded applications, such as for 
+ * systems using uclibc along with posix threading support.  For this reason, 
+ * uCommon disables language features that consume memory or introduce runtime 
+ * overhead, such as rtti and exception handling, and assumes one will mostly 
+ * be linking applications with other pure C based libraries rather than using 
+ * the overhead of the standard C++ library and other class frameworks.
  *
- * UCommon by default does build with support for the bloated ansi standard c++
+ * uCommon by default does build with support for the bloated ansi standard c++
  * library unless this is changed at configure time with the --disable-stdcpp
  * option.  This is to assure maximum portability and will be used to merge
- * UCommon with GNU Common C++ to form GNU Common C++ 2.0.  Some specific
+ * uCommon with GNU Common C++ to form GNU Common C++ 2.0.  Some specific
  * features are tested for when stdc++ is enabled, and these will be used
  * to add back in GNU Common C++ classes such as TCP Stream and serialization.
  *
- * UCommon introduces some Objective-C based design patterns, such as reference
+ * uCommon introduces some Objective-C based design patterns, such as reference
  * counted objects, memory pools, smart pointers, and offers dynamic typing
  * through very light use of inline templates for pure type translation that are
  * then tied to concrete base classes to avoid template instantiation issues.  C++
@@ -47,7 +47,7 @@
  * deleted and threading locks to be released that are acquired automatically when
  * methods return rather than requiring one to explicitly code for these things.
  *
- * UCommon depends on and when necessary will introduce some portable C
+ * uCommon depends on and when necessary will introduce some portable C
  * replacement functions, especially for sockets, such as adding getaddrinfo for
  * platforms which do not have it, or when threadsafe versions of existing C
  * library functions are needed.  Basic socket support for connecting to named
@@ -55,21 +55,21 @@
  * IPV6 addresses is directly supported.  Support for high resolution timing and
  * Posix realtime clocks are also used when available.
  *
- * UCommon builds all higher level thread synchronization objects directly from
+ * uCommon builds all higher level thread synchronization objects directly from
  * conditionals.  Hence, on platforms which for example do not have rwlocks,
- * barriers, or semaphores, these are still found in UCommon.  A common and
+ * barriers, or semaphores, these are still found in uCommon.  A common and
  * consistent call methodology is used for all locks, whether mutex, rw, or
  * semaphore, based on whether used for exclusive or "shared" locking.
  *
- * UCommon requires some knowledge of compiler switches and options to disable
+ * uCommon requires some knowledge of compiler switches and options to disable
  * language features, the C++ runtime and stdlibs, and associated C++ headers. The
  * current version supports compiling with GCC, which is commonly found on
  * GNU/Linux, OS/X, BSD based systems, and many other platforms; and the Sun
- * Workshop compiler, which is offered as an example how to adapt UCommon for
- * additional compilers. UCommon may also be built with GCC cross compiling for
+ * Workshop compiler, which is offered as an example how to adapt uCommon for
+ * additional compilers. uCommon may also be built with GCC cross compiling for
  * mingw32 to build threaded applications for Microsoft Windows targets nativiely.
  *
- * The minimum platform support for UCommon is a modern and working posix 
+ * The minimum platform support for uCommon is a modern and working posix 
  * pthread threading library.  I further use a subset of posix threads to assure 
  * wider portability by avoiding more specialized features like process shared
  * synchronization objects, pthread rwlocks and pthread semaphores, as these are
@@ -77,7 +77,7 @@
  * eliminated the use of posix thread cancellation.
  * @author David Sugar <dyfet@gnutelephony.org>
  * @license GNU Lesser General Public License Version 3 or later
- * @mainpage UCommon
+ * @mainpage GNU uCommon C++
  */
 
 #ifndef	_UCOMMON_UCOMMON_H_
