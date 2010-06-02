9b221cbaae5867705a8a2150c563f8c024b65afd
David Sugar <dyfet@gnutelephony.org>
2010-06-02 01:46:30 +0000

    revised test code

--- test/script.cpp
+++ test/script.cpp
@@ -16,7 +16,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <config.h>
-#include <ccscript.h>
+#include <ucommon/script.h>
 
 using namespace UCOMMON_NAMESPACE;
 
