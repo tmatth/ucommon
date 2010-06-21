dc8ea649cc2729643467beac5e88a1d3c22ea5b8
David Sugar <dyfet@gnutelephony.org>
2010-06-21 11:36:17 +0000

    rollback

--- common/iob.cpp
+++ common/iob.cpp
@@ -564,11 +564,6 @@ size_t fbuf::_push(const char *buf, size_t size)
 #endif
 }
 
-bool fbuf::flush(void)
-{
-	return IOBuffer::flush();
-}
-
 size_t fbuf::_pull(char *buf, size_t size)
 {
 	ssize_t result;
