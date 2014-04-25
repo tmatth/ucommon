c1deee5d228c01c4b5a7f375740660b3cb77998e
David Sugar <dyfet@gnutelephony.org>
2014-04-25 09:39:15 -0400

    some license fixups

--- inc/commoncpp/applog.h
+++ inc/commoncpp/applog.h
@@ -9,9 +9,8 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 // As a special exception, you may use this file as part of a free software
 // library without restriction.  Specifically, if other files instantiate
