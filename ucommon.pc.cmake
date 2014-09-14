ba68090904a219d819d32a61383c50d1f74acfd4
David Sugar <dyfet@gnutelephony.org>
2014-09-14 08:41:16 -0400

    fixed stand-alone cmake usages

--- ucommon.pc.cmake
+++ ucommon.pc.cmake
@@ -6,6 +6,6 @@ minimal="-lucommon ${PACKAGE_LIBS}"
 Name: ${PROJECT_NAME}
 Description: ${PROJECT_NAME} library
 Version: ${PACKAGE_FILE_VERSION}
-Libs: -lusecure -lucommon ${ADDITIONAL_LIBS} ${PACKAGE_LIBS}
-Cflags: ${PACKAGE_FLAGS}
+Libs: -lusecure -lucommon ${ADDITIONAL_LIBS} ${PACKAGE_LIBS} -L${CMAKE_INSTALL_PREFIX}/lib${LIB_SUFFIX}
+Cflags: ${PACKAGE_FLAGS} -I${CMAKE_INSTALL_PREFIX}/include
 
